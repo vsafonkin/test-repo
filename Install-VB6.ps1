79d2a5b4ee5ee00f3d085facf7175d459819a0f9
Vladimir Safonkin <vsafonkin@gmail.com>
2020-08-17 17:46:53 +0300

    Update Install-VB6.ps1

--- Install-VB6.ps1
+++ Install-VB6.ps1
@@ -3,10 +3,9 @@
 New-Item -ItemType "directory" -Path "C:\" -Name "vb6"
 
 $Url = "https://download.microsoft.com/download/5/a/d/5ad868a0-8ecd-4bb0-a882-fe53eb7ef348/VB6.0-KB290887-X86.exe"
-(New-Object System.Net.WebClient).DownloadFile($Url, "C:\vb6\vb6.exe")
+(New-Object System.Net.WebClient).DownloadFile($Url, "C:\vb6.exe")
 
+7z.exe x "C:\vb6.exe" -o"C:\vb6" -y | Out-Null
 Push-Location "C:\vb6"
-# & "C:\\vb6.exe /T:C:\vb6 /C /Q"
-& "vb6.exe /T:C:\vb6 /C /Q"
 
 ls .
