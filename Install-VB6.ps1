b87ed7ed23fa5bed780d3c1660964ec442bedab2
Vladimir Safonkin <vsafonkin@gmail.com>
2020-08-17 14:25:31 +0300

    Update Install-VB6.ps1

--- Install-VB6.ps1
+++ Install-VB6.ps1
@@ -1,5 +1,4 @@
-$Url = "https://www.microsoft.com/en-us/download/confirmation.aspx?id=7030"
-Invoke-WebRequest $Url
+$Url = "https://download.microsoft.com/download/5/6/3/5635D6A9-885E-4C80-A2E7-8A7F4488FBF1/VB60SP6-KB2708437-x86-ENU.msi"
 (New-Object System.Net.WebClient).DownloadFile($Url, "C:\\vb6.msi")
 $ArgumentList = ('/i', 'C:\\vb6.msi', '/QN', '/norestart')
 $filePath = "msiexec.exe"
