4d70b3257a94d20bc8ecec92842439f9f7c9c76b
Vladimir Safonkin <vsafonkin@gmail.com>
2020-08-17 14:44:55 +0300

    Update Install-VB6.ps1

--- Install-VB6.ps1
+++ Install-VB6.ps1
@@ -2,5 +2,21 @@ $Url = "https://download.microsoft.com/download/5/6/3/5635D6A9-885E-4C80-A2E7-8A
 (New-Object System.Net.WebClient).DownloadFile($Url, "C:\\vb6.msi")
 $ArgumentList = ('/i', 'C:\\vb6.msi', '/QN', '/norestart')
 $filePath = "msiexec.exe"
-$process = Start-Process -FilePath $filePath -ArgumentList $ArgumentList -Verb RunAs -Wait -PassThru
-Write-Host $process.ExitCode
+# $process = Start-Process -FilePath $filePath -ArgumentList $ArgumentList -Verb RunAs -Wait -PassThru
+# Write-Host $process.ExitCode
+
+$pinfo = New-Object System.Diagnostics.ProcessStartInfo
+$pinfo.FileName = $filePath
+$pinfo.RedirectStandardError = $true
+$pinfo.RedirectStandardOutput = $true
+$pinfo.UseShellExecute = $false
+$pinfo.Arguments = $ArgumentList
+$p = New-Object System.Diagnostics.Process
+$p.StartInfo = $pinfo
+$p.Start() | Out-Null
+$p.WaitForExit()
+$stdout = $p.StandardOutput.ReadToEnd()
+$stderr = $p.StandardError.ReadToEnd()
+Write-Host "stdout: $stdout"
+Write-Host "stderr: $stderr"
+Write-Host "exit code: " + $p.ExitCode
