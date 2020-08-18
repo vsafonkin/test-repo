a5a526cde470c2003dfa634db69b427f718aec6f
Vladimir Safonkin <vsafonkin@gmail.com>
2020-08-18 15:27:51 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,12 +1,1 @@
-Get-Command bash
-Get-Command gcc
-
 gcc -Wall sample.c -o sample
-
-
-git clone https://github.com/memcached/memcached
-cd memcached
-bash.exe -c "./autogen.sh"
-bash.exe -c "./configure"
-make
-make test
