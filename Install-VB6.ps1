4f56384d2614ac285e2fb6e560222b27bfe34a3c
Vladimir Safonkin <vsafonkin@gmail.com>
2020-08-18 10:08:35 +0300

    Update Install-VB6.ps1

--- Install-VB6.ps1
+++ Install-VB6.ps1
@@ -13,6 +13,8 @@
 
 Get-Command gcc.exe
 Get-Command g++.exe
+Get-Command make.exe
+Get-Command cmake.exe
 
-Write-Host "----------"
+Write-Host "----- C:\ProgramData\Chocolatey\bin -----"
 ls C:\ProgramData\Chocolatey\bin
