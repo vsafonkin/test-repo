8ca09d42d6b249cfbe5fbdbc672af31fc35fbcdb
Vladimir Safonkin <vsafonkin@gmail.com>
2020-08-19 17:11:12 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,5 +1,4 @@
 $origPath = $env:PATH
 $env:PATH = "C:\msys64\mingw64\bin;C:\msys64\usr\bin;$origPath"
-pacman -Syyuu --noconfirm
-taskkill /f /fi "MODULES eq msys-2.0.dll"
+pacman -R --noconfirm --needed --noprogressbar base-devel
 pacman -S --noconfirm --needed --noprogressbar base-devel
