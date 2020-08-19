17ac521a7ab2a58b8eb1fada4da44780ce07b2c5
Vladimir Safonkin <vsafonkin@gmail.com>
2020-08-19 17:37:38 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,5 +1,3 @@
 $origPath = $env:PATH
 $env:PATH = "C:\msys64\mingw64\bin;C:\msys64\usr\bin;$origPath"
-pacman -R compression
-taskkill /f /fi "MODULES eq msys-2.0.dll"
 pacman -S --noconfirm --needed --noprogressbar openssl
