e79fbd16f3595b079ab8f9de4b066e1ff3a4f308
Vladimir Safonkin <vsafonkin@gmail.com>
2020-08-19 15:49:24 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,3 +1,5 @@
-C:\msys64\mingw64\bin\gcc -v -Wall sample.c -o sample
-Write-Host "EXIT CODE: $LASTEXITCODE"
-ls .
+$origPath = $env:PATH
+$env:PATH = "C:\msys64\mingw64\bin;C:\msys64\usr\bin;$origPath"
+Get-Command gcc
+# C:\msys64\mingw64\bin\gcc -v -Wall sample.c -o sample
+# Write-Host "EXIT CODE: $LASTEXITCODE"
