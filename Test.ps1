8affa7391be5783b82f5a8937eb686760bd68632
Vladimir Safonkin <vsafonkin@gmail.com>
2020-08-19 17:42:24 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,3 +1,3 @@
 $origPath = $env:PATH
 $env:PATH = "C:\msys64\mingw64\bin;C:\msys64\usr\bin;$origPath"
-pacman -S --noconfirm --needed --noprogressbar python
+pacman -S --noconfirm --needed --noprogressbar git-extras
