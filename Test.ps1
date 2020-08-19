827c0b10a0073e62172a0c9489af01bf28482278
Vladimir Safonkin <vsafonkin@gmail.com>
2020-08-19 17:16:04 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,4 +1,4 @@
 $origPath = $env:PATH
 $env:PATH = "C:\msys64\mingw64\bin;C:\msys64\usr\bin;$origPath"
-pacman -R --noconfirm --needed --noprogressbar base-devel
+pacman -R --noconfirm base-devel
 pacman -S --noconfirm --needed --noprogressbar base-devel
