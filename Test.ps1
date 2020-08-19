7e90320ae54298bba1233aff89412f7363ccc1d4
Vladimir Safonkin <vsafonkin@gmail.com>
2020-08-19 17:05:22 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -2,4 +2,4 @@ $origPath = $env:PATH
 $env:PATH = "C:\msys64\mingw64\bin;C:\msys64\usr\bin;$origPath"
 pacman -Syyuu --noconfirm
 taskkill /f /fi "MODULES eq msys-2.0.dll"
-pacman -S --noconfirm --needed --noprogressbar base-devel compression
+pacman -S --noconfirm --needed --noprogressbar clang
