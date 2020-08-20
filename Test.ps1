8876ec3609dbeca0d118e2c7869bf8ac0713bf8e
Vladimir Safonkin <vsafonkin@gmail.com>
2020-08-20 11:14:38 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,5 +1,5 @@
 $buildScriptsPackageUrl = 'https://appcenterbuildassets-int.azureedge.net/buildscripts/appcenter-build-assets-latest.zip'
 Invoke-WebRequest -Uri $buildScriptsPackageUrl -OutFile 'appcenter-build-assets-latest.zip'
-New-Item -ItemType directory -Path 'scripts' | Out-Null
+# New-Item -ItemType directory -Path 'scripts' | Out-Null
 unzip -q -d 'scripts' 'appcenter-build-assets-latest.zip'
 if (Test-Path ./scripts) { Invoke-Expression 'bash ./scripts/init.sh' }
