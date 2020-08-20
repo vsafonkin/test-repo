c5643e5922aecaa8125e9b00da4fd36d87850194
Vladimir Safonkin <vsafonkin@gmail.com>
2020-08-20 11:44:16 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,9 +1,4 @@
-# ls $HOME/.gradle
-# Write-Host "----"
-Test-Path ./scripts
-
 $buildScriptsPackageUrl = 'https://appcenterbuildassets-int.azureedge.net/buildscripts/appcenter-build-assets-latest.zip'
 Invoke-WebRequest -Uri $buildScriptsPackageUrl -OutFile 'appcenter-build-assets-latest.zip'
-# New-Item -ItemType directory -Path 'scripts' | Out-Null
-# unzip -q -d 'scripts' 'appcenter-build-assets-latest.zip'
+unzip -q -d 'scripts' 'appcenter-build-assets-latest.zip'
 if (Test-Path ./scripts) { Invoke-Expression 'bash ./scripts/init.sh' }
