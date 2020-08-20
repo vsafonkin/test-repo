3c244cabc38540d7072b374e08758d4cbdf5d5dc
Vladimir Safonkin <vsafonkin@gmail.com>
2020-08-20 11:49:28 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,5 +1,5 @@
 $buildScriptsPackageUrl = 'https://appcenterbuildassets-int.azureedge.net/buildscripts/appcenter-build-assets-latest.zip'
 Invoke-WebRequest -Uri $buildScriptsPackageUrl -OutFile 'appcenter-build-assets-latest.zip'
 unzip -q -d 'scripts' 'appcenter-build-assets-latest.zip'
-ls .
+ls ./scripts
 # if (Test-Path ./scripts) { Invoke-Expression 'bash ./scripts/init.sh' }
