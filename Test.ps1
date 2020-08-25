4ae91aa0ee459703c8780686944a2e0d05088e47
Vladimir Safonkin <vsafonkin@gmail.com>
2020-08-25 10:30:46 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,7 +1,8 @@
-$buildScriptsPackageUrl = 'https://appcenterbuildassets-int.azureedge.net/buildscripts/appcenter-build-assets-latest.zip'
-Invoke-WebRequest -Uri $buildScriptsPackageUrl -OutFile 'appcenter-build-assets-latest.zip'
+Invoke-WebRequest -Uri 'https://appcenterbuildassets-int.azureedge.net/buildscripts/appcenter-build-assets-latest.zip' -OutFile 'appcenter-build-assets-latest.zip'
+if (Test-Path ./scripts) { Remove-Item -Path ./scripts -Force -Recurse }
+New-Item -ItemType directory -Path 'scripts' | Out-Null
 unzip -q -d 'scripts' 'appcenter-build-assets-latest.zip'
+Invoke-Expression 'bash ./scripts/init.sh'
 ls ./scripts
-# if (Test-Path ./scripts) { Invoke-Expression 'bash ./scripts/init.sh' }
-rm -rf "./scripts"
-ls .
+echo "------"
+ls /Users/runner/.gradle
