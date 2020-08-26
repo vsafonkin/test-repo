4bc20208a2485761f8b52a1d4f16f05556726582
Vladimir Safonkin <vsafonkin@gmail.com>
2020-08-26 16:21:19 +0300

    Update Main.hs

--- sample/Main.hs
+++ sample/Main.hs
@@ -1,7 +1,27 @@
 module Main where
 
-import HaskellSay (haskellSay)
+-- import HaskellSay (haskellSay)
 
-main :: IO ()
-main =
-  haskellSay "Azure pipelines canary"
+-- main :: IO ()
+-- main =
+--   haskellSay "Azure pipelines canary"
+
+import Control.Parallel
+import cli-setup
+
+main = a `par` b `par` c `pseq` print (a + b + c)
+    where
+        a = ack 3 10
+        b = fac 42
+        c = fib 34
+
+fac 0 = 1
+fac n = n * fac (n-1)
+
+ack 0 n = n+1
+ack m 0 = ack (m-1) 1
+ack m n = ack (m-1) (ack m (n-1))
+
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
