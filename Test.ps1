1634467dd96492d13ecd4f2a951c8ace486c7f92
Vladimir Safonkin <vsafonkin@gmail.com>
2020-08-26 12:04:40 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -4,23 +4,23 @@ cabal install haskell-say
 ghc Main.hs --make -o sample
 ls .
 
-echo "---------------"
-cd ..
-# cd sample
-# cabal install haskell-say
-# cabal build :sample
+# echo "---------------"
+# cd ..
+# # cd sample
+# # cabal install haskell-say
+# # cabal build :sample
+# # cabal run
+# mkdir sample-cabal
+# cd sample-cabal
+# cabal init
+# cabal build :sample-cabal
 # cabal run
-mkdir sample-cabal
-cd sample-cabal
-cabal init
-cabal build :sample-cabal
-cabal run
 
-echo "---------------"
-cd ..
-stack new sample-stack new-template
-cd sample-stack
-stack setup
-stack build
-stack exec sample-stack-exe
+# echo "---------------"
+# cd ..
+# stack new sample-stack new-template
+# cd sample-stack
+# stack setup
+# stack build
+# stack exec sample-stack-exe
 
