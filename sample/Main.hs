580580441bb11bf6792a24ceeb42c6af72d44c2c
Vladimir Safonkin <vsafonkin@gmail.com>
2020-08-26 16:33:18 +0300

    Update Main.hs

--- sample/Main.hs
+++ sample/Main.hs
@@ -6,11 +6,21 @@ module Main where
 -- main =
 --   haskellSay "Azure pipelines canary"
 
-import           Distribution.CommandLine
-import           Distribution.Simple
+import Control.Parallel
 
-main :: IO ()
-main = setManpath *>
-    writeManpages "man/madlang.1" "madlang.1" *>
-    writeBashCompletions "madlang" *>
-    defaultMain
+main = a `par` b `par` c `pseq` print (a + b + c)
+    where
+        a = ack 3 10
+        b = fac 42
+        c = fib 34
+
+fac 0 = 1
+fac n = n * fac (n-1)
+
+ack 0 n = n+1
+ack m 0 = ack (m-1) 1
+ack m n = ack (m-1) (ack m (n-1))
+
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
