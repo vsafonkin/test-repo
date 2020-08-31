5c72ecb4ed3409e5e7e63a8f53f2726c9ed6137d
Vladimir Safonkin <vsafonkin@gmail.com>
2020-08-31 16:47:50 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,28 +1,4 @@
-# cd sample
-# cabal update
-# cabal install haskell-say
-# ghc Main.hs --make -o sample
-# ls .
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Management-PowerShell
+minikube start
 
-# echo "---------------"
-# cd ..
-cd sample
-cabal update
-cabal install haskell-say
-cabal build :sample
-cabal run
-
-# mkdir sample-cabal
-# cd sample-cabal
-# cabal init
-# cabal build :sample-cabal
-# cabal run
-
-# echo "---------------"
-# cd ..
-# stack new sample-stack new-template
-# cd sample-stack
-# stack setup
-# stack build
-# stack exec sample-stack-exe
 
