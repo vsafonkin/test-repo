5c2d0547110a881a00778eea048d149cef50296d
Vladimir Safonkin <vsafonkin@gmail.com>
2020-08-31 17:22:24 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,4 +1,5 @@
 # Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Management-PowerShell
 minikube start
 minikube status
+kubectl create deployment test-minikube --image=k8s.gcr.io/echoserver:1.10
 
