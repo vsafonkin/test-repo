17e8d4519e3ae2555e1f009e5b0f83c4540d5a69
Vladimir Safonkin <vsafonkin@gmail.com>
2020-08-31 18:09:17 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -5,3 +5,4 @@ kubectl create deployment test-minikube --image=k8s.gcr.io/echoserver:1.10
 kubectl expose deployment test-minikube --type=NodePort --port=8080
 Start-Sleep -Seconds 60
 kubectl get pod
+minikube service test-minikube --url
