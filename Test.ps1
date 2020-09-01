8804bf6d99fcc89541cb72ced7e938d5d818ecea
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-01 09:17:21 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,4 +1,9 @@
 # Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Management-PowerShell
+Import-Module hyper-V
+(Get-Command hyper-v\get-vm).Module.Name
+
+Write-Host "------"
+
 minikube start
 minikube status
 kubectl create deployment test-minikube --image=k8s.gcr.io/echoserver:1.10
