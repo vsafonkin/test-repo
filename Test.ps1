cdeaa2e2343ed440186d547e1269ac377b14ffa2
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-08 11:31:35 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,3 +1,3 @@
-$(phpunit --version) -match "(?<version>\d+\.\d+\.\d+)\s" | Out-Null
+$(phpunit --version | Out-String) -match "(?<version>\d+\.\d+\.\d+)\s" | Out-Null
 Write-Host $Matches.Version
    
