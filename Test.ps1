f85e58817571bad5f3710ab0c1cf6f45d4464c74
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-09 09:48:29 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -2,7 +2,7 @@ cd cppsample
 clang -v -c -Wall -std=c++14 main.cpp
 clang -v -c -Wall -std=c++14 primes_in_range.cpp
 clang -v -c -Wall -std=c++14 primes.cpp
-clang main.o -o primes_in_range
+
 
 echo "--------"
 ls .
