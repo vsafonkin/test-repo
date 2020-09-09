21c06859b62b5b4a6351d53b60b42c850dc83914
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-09 16:41:05 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,3 +1,6 @@
-Get-ChildItem /usr/share | Where-Object { $_ -match "az_\d+" } | Foreach-Object {
-    $_.Name.Split("_")[1]
-}
+# Get-ChildItem /usr/share | Where-Object { $_ -match "az_\d+" } | Foreach-Object {
+#     $_.Name.Split("_")[1]
+# }
+
+hd clone https://github.com/actions/virtual-environments
+cd virtual-environments
