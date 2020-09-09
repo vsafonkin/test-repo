578a74c5ab88458ce531c02f622b2eba297d5d5f
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-09 17:16:38 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,9 +1,4 @@
-# Get-ChildItem /usr/share | Where-Object { $_ -match "az_\d+" } | Foreach-Object {
-#     $_.Name.Split("_")[1]
-# }
-
-hg init -v hello
-cd hello
-echo 'This is a test' > data.txt
-hg add data.txt
+hg init sample-repo
+cp -R . ./sample-repo
+cd sample-repo
 hg status
