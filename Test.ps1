036148c79d76853469ef4ed6909c8c00f92caf56
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-24 00:29:12 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,6 +1,6 @@
 Import-Module -Name SqlServer
 
-Get-ChildItem "C:\Program Files\Microsoft SQL Server\150"
+Get-ChildItem "C:\Program Files\Microsoft SQL Server\150\DAC"
 
 Set-Location "C:\Program Files\Microsoft SQL Server\130\Tools\Binn"
 SqlLocalDB.exe create "TestLocalDBInstance"
