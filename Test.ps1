093ede3ceb8fb651a60c93784ff9177acec567a2
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-23 23:10:11 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,10 +1,13 @@
 Set-Location "C:\Program Files\Microsoft SQL Server\130\Tools\Binn"
-SqlLocalDB.exe create "TestLocalDBInstance"
-SqlLocalDB.exe share "TestLocalDBInstance" "TestSharedLocalDBInstance"
-SqlLocalDB.exe start "TestLocalDBInstance"  
-SqlLocalDB.exe info "TestLocalDBInstance"
+# SqlLocalDB.exe create "TestLocalDBInstance"
+# SqlLocalDB.exe share "TestLocalDBInstance" "TestSharedLocalDBInstance"
+# SqlLocalDB.exe start "TestLocalDBInstance"  
+# SqlLocalDB.exe info "TestLocalDBInstance"
 
-sqlcmd -S (localDB)\TestLocalDBInstance
+SqlLocalDB.exe start
+SqlLocalDB.exe info
+
+# sqlcmd -S (localDB)\TestLocalDBInstance
 # Invoke-Sqlcmd -Query "SELECT GETDATE() AS TimeOfQuery" -ServerInstance "TestSharedLocalDBInstance"
 
 # Set-Location "C:\Users\runneradmin\AppData\Local\Microsoft\Microsoft SQL Server Local DB\Instances\TestLocalDBInstance"
