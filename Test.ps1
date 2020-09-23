b2d2bbaba719e8a063b23a9885f68085ef43eaaf
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-24 00:21:08 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -10,7 +10,7 @@ SqlLocalDB.exe info "TestLocalDBInstance"
 # SqlLocalDB.exe info MSSQLLocalDB
 
 Get-PSDrive
-Get-ChildItem "SQLSERVER:\SQL\fv-az153"
+Get-ChildItem "SQLSERVER:\SQL\fv-az153\TestLocalDBInstance"
 # Get-SqlInstance -Credential -MachineName "fv-az153"
 # Invoke-Sqlcmd -Query "SELECT GETDATE() AS TimeOfQuery" -ServerInstance "fv-az153\MSSQLLocalDB"
 Set-Location "SQLSERVER:\SQL\fv-az153"
