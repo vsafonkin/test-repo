08623c84d71b4a015a498483506123279dcf1261
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-23 22:14:05 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,5 +1,6 @@
 Set-Location "C:\Program Files\Microsoft SQL Server\130\Tools\Binn"
-SqlLocalDB.exe create "TestLocalDBInstance" 
+SqlLocalDB.exe create "TestLocalDBInstance"
+SqlLocalDB.exe share "TestLocalDBInstance" "TestSharedLocalDBInstance"
 SqlLocalDB.exe start "TestLocalDBInstance"  
 SqlLocalDB.exe info "TestLocalDBInstance"
 
