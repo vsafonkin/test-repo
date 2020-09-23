09b838622a3f83c1c65b6c729934d47182577f8a
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-23 18:04:20 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -3,7 +3,7 @@ SqlLocalDB.exe create "TestLocalDBInstance"
 SqlLocalDB.exe start "TestLocalDBInstance"  
 SqlLocalDB.exe info "TestLocalDBInstance"
 
-Invoke-Sqlcmd -Query "SELECT GETDATE() AS TimeOfQuery" -ServerInstance "TestLocalDBInstance"
+Invoke-Sqlcmd -Query "SELECT GETDATE() AS TimeOfQuery" -ServerInstance "np:\\.\pipe\LOCALDB#88D853C0\tsql\query"
 
 # Set-Location "C:\Users\runneradmin\AppData\Local\Microsoft\Microsoft SQL Server Local DB\Instances\TestLocalDBInstance"
 # Invoke-Sqlcmd -Query "SELECT GETDATE() AS TimeOfQuery" -ServerInstance "TestLocalDBInstance"
