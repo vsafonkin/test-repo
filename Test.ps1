f3c119719743a6f8c41152b9687b7a77b29e1186
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-24 10:36:48 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,7 +1,4 @@
-# Import-Module -Name SqlServer
-
 SqlLocalDB.exe create "TestLocalDBInstance"
-# SqlLocalDB.exe share "TestLocalDBInstance" "TestSharedLocalDBInstance"
 SqlLocalDB.exe start "TestLocalDBInstance"  
 SqlLocalDB.exe info "TestLocalDBInstance"
 
