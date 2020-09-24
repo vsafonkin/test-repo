4c003f4140d4e02d324c0b94e2d9a853bc04b19d
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-24 10:19:08 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -7,4 +7,16 @@ SqlLocalDB.exe info "TestLocalDBInstance"
 
 sqlcmd -S "(localdb)\TestLocalDBInstance" -Q "SELECT GETDATE() AS TimeOfQuery"
 sqlcmd -S "(localdb)\TestLocalDBInstance" -Q "CREATE DATABASE testDatabase"
-sqlcmd -S "(localdb)\TestLocalDBInstance" -d "testDatabase" -Q "CREATE Table testTable"
+
+$createTestTableQuery = @"
+CREATE TABLE testTable (
+    visit_id INT PRIMARY KEY IDENTITY (1, 1),
+    first_name VARCHAR (50) NOT NULL,
+    last_name VARCHAR (50) NOT NULL,
+    visited_at DATETIME,
+    phone VARCHAR(20),
+    store_id INT NOT NULL,
+    FOREIGN KEY (store_id) REFERENCES sales.stores (store_id)
+);
+"@
+sqlcmd -S "(localdb)\TestLocalDBInstance" -d "testDatabase" -Q $createTestTableQuery
