03579e0a57e490bdadbb23515de6dd9d2b55e96b
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-24 10:21:10 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -15,8 +15,8 @@ CREATE TABLE testTable (
     last_name VARCHAR (50) NOT NULL,
     visited_at DATETIME,
     phone VARCHAR(20),
-    store_id INT NOT NULL,
-    FOREIGN KEY (store_id) REFERENCES sales.stores (store_id)
+    store_id INT NOT NULL
 );
 "@
+
 sqlcmd -S "(localdb)\TestLocalDBInstance" -d "testDatabase" -Q $createTestTableQuery
