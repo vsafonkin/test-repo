bbe65bff48d7b5c725d078173a62c4abc06070c8
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-24 13:17:54 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,5 +1,5 @@
 (Get-Command sqlcmd).Source
-Get-ChildItem "C:\Program Files\Microsoft SQL Server\Client SDK\ODBC"
+Get-ChildItem "C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\170"
 
 SqlLocalDB.exe create "TestLocalDBInstance"
 SqlLocalDB.exe start "TestLocalDBInstance"  
