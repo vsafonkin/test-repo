b62b5e226bc7eb705854459b0a1b6fbc918d011b
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-29 16:46:20 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -3,4 +3,4 @@ Import-Module .\helpers.psm1 -DisableNameChecking
 Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/download/toolchain.msi" -Name "toolchain.msi"
 Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/download/sdk.msi" -Name "sdk.msi"
 
-Write-Host $env:SDKROOT
+Get-ChildItem %SystemDrive%\Library\Developer\Platforms\Windows.platform\Developer\SDKs
