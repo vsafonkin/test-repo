e617a392bd8d7f2be0ae65689b4bddb7e01c9071
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-29 16:27:25 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,3 +1,4 @@
 Import-Module .\helpers.psm1 -DisableNameChecking
 
-Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/download/installer.exe" -Name "installer.exe"
+Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/download/toolchain.msi" -Name "toolchain.msi"
+Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/download/sdk.msi" -Name "sdk.msi"
