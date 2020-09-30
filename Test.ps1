00198d9be7dd32e5af4cc5259d6bf189b5124f4c
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-30 15:17:55 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -3,22 +3,17 @@ Import-Module .\helpers.psm1 -DisableNameChecking
 # Start-Process -FilePath "$env:comspec" -ArgumentList "/c echo %SystemDrive%" -PassThru -Wait -Verb RunAs
 # Get-ChildItem "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build"
 
-try {
-    $MyProcess = New-Object System.Diagnostics.Process
+
+$MyProcess = New-Object System.Diagnostics.Process
     
-    $MyProcess.StartInfo.FileName = "$env:comspec"
-    $MyProcess.StartInfo.UseShellExecute = $false
-    $MyProcess.StartInfo.RedirectStandardInput = $true
-#     $MyProcess.StartInfo.RedirectStandardOutput = $true
-    $MyProcess.Start() | Out-Null
-    $StdIn = $MyProcess.StandardInput
-    $StdIn.WriteLine("echo %APPDATA%")
-    $StdIn.WriteLine("echo %SystemDrive%")
-} finally {
-    if($StdIn) {
-      $StdIn.Close()
-    }
-  }
+$MyProcess.StartInfo.FileName = "$env:comspec"
+$MyProcess.StartInfo.UseShellExecute = $false
+$MyProcess.StartInfo.RedirectStandardInput = $true
+$MyProcess.Start() | Out-Null
+$StdIn = $MyProcess.StandardInput
+$StdIn.WriteLine("echo %APPDATA%")
+$StdIn.WriteLine("echo %SystemDrive%")
+$StdIn.Close()
 
 # Write-Host "-------"
 # Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/download/toolchain.msi" -Name "toolchain.msi"
