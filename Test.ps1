1dc703c40e06737d917a3a9d428edfeaedec026c
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-30 13:02:10 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,9 +1,9 @@
 Import-Module .\helpers.psm1 -DisableNameChecking
 
-$env:comspec
+Start-Process -FilePath "$env:comspec" -ArgumentList "%SystemDrive%" -Verb RunAs
 # Get-ChildItem "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build"
 
-
+Write-Host "-------"
 Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/download/toolchain.msi" -Name "toolchain.msi"
 Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/download/sdk.msi" -Name "sdk.msi"
 
