bbf5e6e8cf1ed505f6b206b84693dc08195cb183
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-30 12:41:33 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -8,5 +8,5 @@ Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/dow
 Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/download/sdk.msi" -Name "sdk.msi"
 
 $commandLineNativeTools = "C:\Program-ArgumentList "echo"," Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
-Start-Process -FilePath $commandLineNativeTools -ArgumentList "echo","%UniversalCRTSdkDir%" -Verb RunAs
+Start-Process -FilePath $commandLineNativeTools -ArgumentList "echo %UniversalCRTSdkDir%" -Verb RunAs
 
