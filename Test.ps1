287f9f0af9397a3d0cd3c9fe17459a5f8c548b3c
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-30 10:37:56 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,7 +1,7 @@
 Import-Module .\helpers.psm1 -DisableNameChecking
 
 
-Get-ChildItem "C:\Program Files (x86)\Microsoft Visual Studio\2019"
+Get-ChildItem "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise"
 
 
 Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/download/toolchain.msi" -Name "toolchain.msi"
