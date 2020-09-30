953b79bcb5be9c9cedcb87596c8765b8ab27047a
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-30 15:56:23 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -21,7 +21,7 @@ Import-Module .\helpers.psm1 -DisableNameChecking
 # Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/download/sdk.msi" -Name "sdk.msi"
 
 $commandLineNativeTools = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
-& $commandLineNativeTools
+# & $commandLineNativeTools
 
 # $pinfo = New-Object System.Diagnostics.ProcessStartInfo
 # $pinfo.FileName = $commandLineNativeTools
@@ -45,20 +45,20 @@ $commandLineNativeTools = "C:\Program Files (x86)\Microsoft Visual Studio\2019\E
 # cmd.exe -c "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 
 
-# $MyProcess = New-Object System.Diagnostics.Process
+$MyProcess = New-Object System.Diagnostics.Process
     
-# $MyProcess.StartInfo.FileName = $commandLineNativeTools
-# $MyProcess.StartInfo.UseShellExecute = $false
-# $MyProcess.StartInfo.RedirectStandardInput = $true
-# $MyProcess.StartInfo.RedirectStandardOutput = $true
-# $MyProcess.StartInfo.Verb = "RunAs"
-# $MyProcess.Start() | Out-Null
-# Start-Sleep -s 10
-# $StdIn = $MyProcess.StandardInput
-# $StdOut = $MyProcess.StandardOutput
-# $StdIn.WriteLine("echo %APPDATA%")
-# $StdIn.WriteLine("echo %SystemDrive%")
-# $StdIn.Close()
+$MyProcess.StartInfo.FileName = $commandLineNativeTools
+$MyProcess.StartInfo.UseShellExecute = $true
+$MyProcess.StartInfo.RedirectStandardInput = $true
+$MyProcess.StartInfo.RedirectStandardOutput = $true
+$MyProcess.StartInfo.Verb = "RunAs"
+$MyProcess.Start() | Out-Null
+Start-Sleep -s 10
+$StdIn = $MyProcess.StandardInput
+$StdOut = $MyProcess.StandardOutput
+$StdIn.WriteLine("echo %APPDATA%")
+$StdIn.WriteLine("echo %SystemDrive%")
+$StdIn.Close()
 
-# $output = $StdOut.ReadToEnd()
-# $output
+$output = $StdOut.ReadToEnd()
+$output
