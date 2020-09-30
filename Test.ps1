4c39118d9e61d1bd1f04b2bea25ab8d8e8aeba65
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-30 11:53:17 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -6,3 +6,6 @@ Get-ChildItem "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC
 
 Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/download/toolchain.msi" -Name "toolchain.msi"
 Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/download/sdk.msi" -Name "sdk.msi"
+
+$commandLineNativeTools = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+Start-Process -FilePath $commandLineNativeTools -Verb RunAs
