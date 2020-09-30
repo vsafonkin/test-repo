fddf1378049710090a9cddab0ba034c170a6a1b2
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-30 09:05:31 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,6 +1,6 @@
 Import-Module .\helpers.psm1 -DisableNameChecking
 
-Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/download/toolchain.msi" -Name "toolchain.msi"
-Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/download/sdk.msi" -Name "sdk.msi"
-
+# Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/download/toolchain.msi" -Name "toolchain.msi"
+# Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/download/sdk.msi" -Name "sdk.msi"
+Install-Binary -Url "https://swift.org/builds/swift-5.3-release/windows10/swift-5.3-RELEASE/swift-5.3-RELEASE-windows10.exe" -Name "swift-5.3-RELEASE-windows10.exe"
 swift --version
