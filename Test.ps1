1e5968f8ae64f00cd19e9f36ba1e04ec42f7eb95
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-30 15:19:33 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -4,16 +4,16 @@ Import-Module .\helpers.psm1 -DisableNameChecking
 # Get-ChildItem "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build"
 
 
-$MyProcess = New-Object System.Diagnostics.Process
+# $MyProcess = New-Object System.Diagnostics.Process
     
-$MyProcess.StartInfo.FileName = "$env:comspec"
-$MyProcess.StartInfo.UseShellExecute = $false
-$MyProcess.StartInfo.RedirectStandardInput = $true
-$MyProcess.Start() | Out-Null
-$StdIn = $MyProcess.StandardInput
-$StdIn.WriteLine("echo %APPDATA%")
-$StdIn.WriteLine("echo %SystemDrive%")
-$StdIn.Close()
+# $MyProcess.StartInfo.FileName = "$env:comspec"
+# $MyProcess.StartInfo.UseShellExecute = $false
+# $MyProcess.StartInfo.RedirectStandardInput = $true
+# $MyProcess.Start() | Out-Null
+# $StdIn = $MyProcess.StandardInput
+# $StdIn.WriteLine("echo %APPDATA%")
+# $StdIn.WriteLine("echo %SystemDrive%")
+# $StdIn.Close()
 
 # Write-Host "-------"
 # Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/download/toolchain.msi" -Name "toolchain.msi"
