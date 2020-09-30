46c0f8df088e17fdde80835b37c925e0afa62dbb
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-30 13:49:46 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,22 +1,22 @@
 Import-Module .\helpers.psm1 -DisableNameChecking
 
-Start-Process -FilePath "$env:comspec" -ArgumentList "/c echo %SystemDrive%" -PassThru -Wait -Verb RunAs
+# Start-Process -FilePath "$env:comspec" -ArgumentList "/c echo %SystemDrive%" -PassThru -Wait -Verb RunAs
 # Get-ChildItem "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build"
 
-$pinfo = New-Object System.Diagnostics.ProcessStartInfo
-$pinfo.FileName = "$env:comspec"
-$pinfo.Arguments = "/c", "echo", "%APPDATA%"
-$pinfo.RedirectStandardError = $true
-$pinfo.RedirectStandardOutput = $true
-$pinfo.UseShellExecute = $false
-$pinfo.Verb = "RunAs"
-$p = New-Object System.Diagnostics.Process
-$p.StartInfo = $pinfo
-$p.Start() | Out-Null
-$p.WaitForExit()
-$output = $p.StandardOutput.ReadToEnd()
-$output += $p.StandardError.ReadToEnd()
-$output
+# $pinfo = New-Object System.Diagnostics.ProcessStartInfo
+# $pinfo.FileName = "$env:comspec"
+# $pinfo.Arguments = "/c", "echo", "%APPDATA%"
+# $pinfo.RedirectStandardError = $true
+# $pinfo.RedirectStandardOutput = $true
+# $pinfo.UseShellExecute = $false
+# $pinfo.Verb = "RunAs"
+# $p = New-Object System.Diagnostics.Process
+# $p.StartInfo = $pinfo
+# $p.Start() | Out-Null
+# $p.WaitForExit()
+# $output = $p.StandardOutput.ReadToEnd()
+# $output += $p.StandardError.ReadToEnd()
+# $output
 
 Write-Host "-------"
 Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/download/toolchain.msi" -Name "toolchain.msi"
