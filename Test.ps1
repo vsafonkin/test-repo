f10a7c7d5993098d43d2bb1aa5d7fe9015428b56
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-30 12:48:14 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -10,3 +10,5 @@ Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/dow
 $commandLineNativeTools = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 Start-Process -FilePath $commandLineNativeTools -ArgumentList "echo %UniversalCRTSdkDir%" -Verb RunAs
 
+cmd.exe -c "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+echo %SDKROOT%
