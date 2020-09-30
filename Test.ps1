51522cea31cbfe55e30cab112ce6aa8c5d45a08c
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-30 14:04:43 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -27,7 +27,7 @@ $commandLineNativeTools = "C:\Program Files (x86)\Microsoft Visual Studio\2019\E
 
 $pinfo = New-Object System.Diagnostics.ProcessStartInfo
 $pinfo.FileName = $commandLineNativeTools
-$pinfo.Arguments = "/c", "echo", "%APPDATA%"
+# $pinfo.Arguments = "/c", "echo", "%APPDATA%"
 $pinfo.RedirectStandardError = $true
 $pinfo.RedirectStandardOutput = $true
 $pinfo.UseShellExecute = $false
@@ -35,6 +35,8 @@ $pinfo.Verb = "RunAs"
 $p = New-Object System.Diagnostics.Process
 $p.StartInfo = $pinfo
 $p.Start() | Out-Null
+Start-Sleep -s 2
+$p.StandardInput.WriteLine("echo %APPDATA%");
 $p.WaitForExit()
 $output = $p.StandardOutput.ReadToEnd()
 $output += $p.StandardError.ReadToEnd()
@@ -42,3 +44,4 @@ $output
 
 # Start-Process -FilePath $commandLineNativeTools -ArgumentList "/c echo %SystemDrive%" -PassThru -Wait -Verb RunAs
 # cmd.exe -c "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+
