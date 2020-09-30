d8613a7b1b031dca008743b7d9bd4cac2a8e0d9f
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-30 12:28:45 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,11 +1,13 @@
 Import-Module .\helpers.psm1 -DisableNameChecking
 
 
-Get-ChildItem "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build"
+# Get-ChildItem "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build"
 
 
 Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/download/toolchain.msi" -Name "toolchain.msi"
 Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/download/sdk.msi" -Name "sdk.msi"
 
-$commandLineNativeTools = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
-Start-Process -FilePath $commandLineNativeTools -Verb RunAs | echo %UniversalCRTSdkDir%
+# $commandLineNativeTools = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+# Start-Process -FilePath $commandLineNativeTools -Verb RunAs | echo %UniversalCRTSdkDir%
+
+Get-ChildItem "$env:SDKROOT/usr/lib
