4b957714de8ecb712c55c250ee337fa6adb4da35
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-30 13:25:23 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,4 +1,5 @@
 Import-Module .\helpers.psm1 -DisableNameChecking
+cmd.exe -c "/c echo %APPDATA%"
 Start-Process -FilePath "$env:comspec" -ArgumentList "/c echo %SystemDrive%" -Verb RunAs
 # Get-ChildItem "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build"
 
@@ -7,7 +8,7 @@ Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/dow
 Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/download/sdk.msi" -Name "sdk.msi"
 
 $commandLineNativeTools = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
-Start-Process -FilePath $commandLineNativeTools -ArgumentList "echo %UniversalCRTSdkDir%" -Verb RunAs
+Start-Process -FilePath $commandLineNativeTools -ArgumentList "/c echo %SystemDrive%" -Verb RunAs
 
 # cmd.exe -c "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 
