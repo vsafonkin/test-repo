34be9899ee72e43e4dd2c6bba48ff70a84a75f07
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-30 16:56:04 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,49 +1,10 @@
 Import-Module .\helpers.psm1 -DisableNameChecking
 
-# Start-Process -FilePath "$env:comspec" -ArgumentList "/c echo %SystemDrive%" -PassThru -Wait -Verb RunAs
-# Get-ChildItem "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build"
 
-
-# $MyProcess = New-Object System.Diagnostics.Process
-    
-# $MyProcess.StartInfo.FileName = "$env:comspec"
-# $MyProcess.StartInfo.UseShellExecute = $false
-# $MyProcess.StartInfo.RedirectStandardInput = $true
-# $MyProcess.StartInfo.Verb = "RunAs"
-# $MyProcess.Start() | Out-Null
-# $StdIn = $MyProcess.StandardInput
-# $StdIn.WriteLine("echo %APPDATA%")
-# $StdIn.WriteLine("echo %SystemDrive%")
-# $StdIn.Close()
-
-# Write-Host "-------"
-# Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/download/toolchain.msi" -Name "toolchain.msi"
-# Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/download/sdk.msi" -Name "sdk.msi"
+Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/download/toolchain.msi" -Name "toolchain.msi"
+Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/download/sdk.msi" -Name "sdk.msi"
 
 $commandLineNativeTools = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
-# & $commandLineNativeTools
-
-# $pinfo = New-Object System.Diagnostics.ProcessStartInfo
-# $pinfo.FileName = $commandLineNativeTools
-# # $pinfo.Arguments = "/c", "echo", "%APPDATA%"
-# $pinfo.RedirectStandardError = $true
-# $pinfo.RedirectStandardOutput = $true
-# $pinfo.RedirectStandardInput = $true
-# $pinfo.UseShellExecute = $false
-# $pinfo.Verb = "RunAs"
-# $p = New-Object System.Diagnostics.Process
-# $p.StartInfo = $pinfo
-# $p.Start() | Out-Null
-# Start-Sleep -s 2
-# $p.StandardInput.WriteLine("echo %APPDATA%");
-# # $p.WaitForExit()
-# $output = $p.StandardOutput.ReadToEnd()
-# $output += $p.StandardError.ReadToEnd()
-# $output
-
-# Start-Process -FilePath $commandLineNativeTools -ArgumentList "/c echo %SystemDrive%" -PassThru -Wait -Verb RunAs
-# cmd.exe -c "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
-
 
 $MyProcess = New-Object System.Diagnostics.Process
     
@@ -58,13 +19,13 @@ $StdIn.WriteLine("SET > C:\vcvars.txt")
 $StdIn.Close()
 $MyProcess.WaitForExit()
 
-
-Get-Content "C:\vcvars.txt"
-
 Get-Content "C:\vcvars.txt" | Foreach-Object {
   if ($_ -match "^(.*?)=(.*)$") {
     Set-Content "env:\$($matches[1])" $matches[2]
   }
 }
 
-env
+copy $env:SDKROOT\usr\share\ucrt.modulemap "$env:UniversalCRTSdkDir\Include\$env:UCRTVersion\ucrt\module.modulemap"
+copy $env:SDKROOT\usr\share\visualc.modulemap "$env:VCToolsInstallDir\include\module.modulemap"
+copy $env:SDKROOT\usr\share\visualc.apinotes "$env:VCToolsInstallDir\include\visualc.apinotes"
+copy $env:SDKROOT\usr\share\winsdk.modulemap "$env:UniversalCRTSdkDir\Include\$env:UCRTVersion\um\module.modulemap"
