5e3f28ddea0efc798bc8b43953fa2edd5ba0c70a
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-30 13:49:03 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,6 +1,5 @@
 Import-Module .\helpers.psm1 -DisableNameChecking
 
-cmd.exe -c "/c echo %APPDATA%"
 Start-Process -FilePath "$env:comspec" -ArgumentList "/c echo %SystemDrive%" -PassThru -Wait -Verb RunAs
 # Get-ChildItem "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build"
 
@@ -24,5 +23,22 @@ Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/dow
 Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/download/sdk.msi" -Name "sdk.msi"
 
 $commandLineNativeTools = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
-Start-Process -FilePath $commandLineNativeTools -ArgumentList "/c echo %SystemDrive%" -PassThru -Wait -Verb RunAs
+
+
+$pinfo = New-Object System.Diagnostics.ProcessStartInfo
+$pinfo.FileName = $commandLineNativeTools
+$pinfo.Arguments = "/c", "echo", "%APPDATA%"
+$pinfo.RedirectStandardError = $true
+$pinfo.RedirectStandardOutput = $true
+$pinfo.UseShellExecute = $false
+$pinfo.Verb = "RunAs"
+$p = New-Object System.Diagnostics.Process
+$p.StartInfo = $pinfo
+$p.Start() | Out-Null
+$p.WaitForExit()
+$output = $p.StandardOutput.ReadToEnd()
+$output += $p.StandardError.ReadToEnd()
+$output
+
+# Start-Process -FilePath $commandLineNativeTools -ArgumentList "/c echo %SystemDrive%" -PassThru -Wait -Verb RunAs
 # cmd.exe -c "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
