4b37d4cfadc0e4ecae76d23edbdd95524eb9dbe8
Vladimir Safonkin <vsafonkin@gmail.com>
2020-09-30 15:24:34 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -4,17 +4,17 @@ Import-Module .\helpers.psm1 -DisableNameChecking
 # Get-ChildItem "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build"
 
 
-$MyProcess = New-Object System.Diagnostics.Process
+# $MyProcess = New-Object System.Diagnostics.Process
     
-$MyProcess.StartInfo.FileName = "$env:comspec"
-$MyProcess.StartInfo.UseShellExecute = $false
-$MyProcess.StartInfo.RedirectStandardInput = $true
-$MyProcess.StartInfo.Verb = "RunAs"
-$MyProcess.Start() | Out-Null
-$StdIn = $MyProcess.StandardInput
-$StdIn.WriteLine("echo %APPDATA%")
-$StdIn.WriteLine("echo %SystemDrive%")
-$StdIn.Close()
+# $MyProcess.StartInfo.FileName = "$env:comspec"
+# $MyProcess.StartInfo.UseShellExecute = $false
+# $MyProcess.StartInfo.RedirectStandardInput = $true
+# $MyProcess.StartInfo.Verb = "RunAs"
+# $MyProcess.Start() | Out-Null
+# $StdIn = $MyProcess.StandardInput
+# $StdIn.WriteLine("echo %APPDATA%")
+# $StdIn.WriteLine("echo %SystemDrive%")
+# $StdIn.Close()
 
 # Write-Host "-------"
 # Install-Binary -Url "https://github.com/compnerd/swift-build/releases/latest/download/toolchain.msi" -Name "toolchain.msi"
@@ -44,19 +44,15 @@ $commandLineNativeTools = "C:\Program Files (x86)\Microsoft Visual Studio\2019\E
 # Start-Process -FilePath $commandLineNativeTools -ArgumentList "/c echo %SystemDrive%" -PassThru -Wait -Verb RunAs
 # cmd.exe -c "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 
-# try {
-#     $MyProcess = New-Object System.Diagnostics.Process
+
+$MyProcess = New-Object System.Diagnostics.Process
     
-#     $MyProcess.StartInfo.FileName = $commandLineNativeTools
-#     $MyProcess.StartInfo.UseShellExecute = $false
-#     $MyProcess.StartInfo.RedirectStandardInput = $true
-#     $MyProcess.StartInfo.Verb = "RunAs"
-#     $MyProcess.Start() | Out-Null
-#     $StdIn = $MyProcess.StandardInput
-#     $StdIn.WriteLine("echo %APPDATA%")
-#     $StdIn.WriteLine("echo %SystemDrive%")
-# } finally {
-#     if($StdIn) {
-#       $StdIn.Close()
-#     }
-#   }
+$MyProcess.StartInfo.FileName = $commandLineNativeTools
+$MyProcess.StartInfo.UseShellExecute = $false
+$MyProcess.StartInfo.RedirectStandardInput = $true
+$MyProcess.StartInfo.Verb = "RunAs"
+$MyProcess.Start() | Out-Null
+$StdIn = $MyProcess.StandardInput
+$StdIn.WriteLine("echo %APPDATA%")
+$StdIn.WriteLine("echo %SystemDrive%")
+$StdIn.Close()
