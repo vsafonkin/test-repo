9889c33f2800bea5e5365cd0791a9f27b3789da4
Vladimir Safonkin <vsafonkin@gmail.com>
2020-10-02 11:05:23 +0300

    Update pipx_version.ps1

--- pipx_version.ps1
+++ pipx_version.ps1
@@ -1,5 +1,21 @@
+function Get-CommandResult {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string] $Command,
+        [switch] $Multiline
+    )
+    # Bash trick to suppress and show error output because some commands write to stderr (for example, "python --version")
+    $stdout = & bash -c "$Command 2>&1"
+    $exitCode = $LASTEXITCODE
+    return @{
+        Output = If ($Multiline -eq $true) { $stdout } else { [string]$stdout }
+        ExitCode = $exitCode
+    }
+}
+
 function Get-PipxVersion {
-    return "Pipx $((pipx --version | Out-String).Split(" ")[0])"
+    $pipxVersion = Get-CommandResult "pipx --version").Output
+    return "Pipx $pipxVersion"
 }
 
 Get-PipxVersion
