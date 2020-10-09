644306316149a8ffb1e57ef616471963d786e3b9
Vladimir Safonkin <vsafonkin@gmail.com>
2020-10-09 15:51:09 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,9 +2,6 @@ echo "clone repository"
 git clone https://github.com/couchbaselabs/couchbase-shell
 cd ./couchbase-shell
 
-echo "install deps"
-brew install automake
-
 export MACOSX_DEPLOYMENT_TARGET=10.9
 export VCPKGRS_DYNAMIC=1
 
