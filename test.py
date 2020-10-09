395c96d04f8e43c63959d6041f6d52149de65510
Vladimir Safonkin <vsafonkin@gmail.com>
2020-10-09 16:34:52 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import requests
 
-url = "https://github.com/actions/virtual-environments/issues/1745"
+url = "https://github.com/aws/aws-sam-cli/releases/download/v1.6.2/aws-sam-cli-1.6.2.sierra.bottle.tar.gz"
 with requests.get(url, allow_redirects=True) as response, open("python_log.txt", "wb") as output:
         output.write(response.content)
