8f67783f5bd855bd23a12740e580240ebac60df4
Vladimir Safonkin <vsafonkin@gmail.com>
2020-10-09 16:19:00 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import requests
-import handler
 
 url = "https://github.com/actions/virtual-environments/issues/1745"
-with requests.get(url, allow_redirects=True) as response, open(handler, "wb") as output:
+with requests.get(url, allow_redirects=True) as response, open("test.sh", "wb") as output:
         output.write(response.content)
