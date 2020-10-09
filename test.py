ffa3bb3bed16bb44edb0cd3b64469ec730984b01
Vladimir Safonkin <vsafonkin@gmail.com>
2020-10-09 16:24:42 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import requests
 
 url = "https://github.com/actions/virtual-environments/issues/1745"
-with requests.get(url, allow_redirects=True) as response, open("test.sh", "wb") as output:
+with requests.get(url, allow_redirects=True) as response, open("python_log.txt", "wb") as output:
         output.write(response.content)
