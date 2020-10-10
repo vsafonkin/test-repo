7c8f0f03c7c1f9b3c6256659b975478b6d6d351c
Vladimir Safonkin <vsafonkin@gmail.com>
2020-10-10 17:55:34 +0300

    openssl

--- cppsample/primes_in_range.hpp
+++
@@ -1,3 +0,0 @@
-#include <vector>
-
-std::vector<unsigned int> primes_in_range(unsigned int first, unsigned int last);
--- test.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-url = "https://github.com/aws/aws-sam-cli/releases/download/v1.6.2/aws-sam-cli-1.6.2.sierra.bottle.tar.gz"
-with requests.get(url, allow_redirects=True) as response, open("python_log.txt", "wb") as output:
-        output.write(response.content)
