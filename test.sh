5f3def5bb198a0d482a95d68bae388df9a0045af
Vladimir Safonkin <vsafonkin@gmail.com>
2020-10-12 16:46:38 +0300

    Xcode command line tools

--- test.sh
+++ test.sh
@@ -1,16 +1,18 @@
 install_clt() {
     echo "Searching online for the Command Line Tools"
     # This temporary file prompts the 'softwareupdate' utility to list the Command Line Tools
-    clt_placeholder="/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
-    sudo touch $clt_placeholder
+    # clt_placeholder="/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
+    # sudo touch $clt_placeholder
     clt_label_command="/usr/sbin/softwareupdate -l |
                         grep -B 1 -E 'Command Line Tools' |
                         awk -F'*' '/^ *\\*/ {print \$2}' |
                         sed -e 's/^ *Label: //' -e 's/^ *//' |
                         sort -V |
                         tail -n1"
+
     retries=2
     sleepInterval=3
+
     until [[ $retries -le 0 ]]; do
         clt_label=$(eval $clt_label_command)
         if [[ -z "$clt_label" ]]; then
@@ -29,12 +31,9 @@ install_clt() {
         echo "Unable to find command line tools, wait for $sleepInterval seconds, $retries attempts left"
         sleep $sleepInterval
     done
-    # if [[ -n "$clt_label" ]]; then
-    # echo "Installing $clt_label"
-    # sudo "/usr/sbin/softwareupdate" "-i" "$clt_label"
+
     # sudo "/bin/rm" "-f" "$clt_placeholder"
-    # sudo "/usr/bin/xcode-select" "--switch" "/Library/Developer/CommandLineTools"
-    # fi
+    sudo "/usr/bin/xcode-select" "--switch" "/Library/Developer/CommandLineTools"
 }
 
 install_clt
