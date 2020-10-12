7b3b6b0222e5ca6d2f443b34429ca44e7f95d1ab
Vladimir Safonkin <vsafonkin@gmail.com>
2020-10-12 11:20:42 +0300

    Test pipx version

--- Test.ps1
+++ Test.ps1
@@ -1,2 +1,22 @@
+function Get-CommandResult {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string] $Command,
+        [switch] $Multiline
+    )
+    # Bash trick to suppress and show error output because some commands write to stderr (for example, "python --version")
+    $stdout = & bash -c "$Command 2>&1"
+    $exitCode = $LASTEXITCODE
+    return @{
+        Output = If ($Multiline -eq $true) { $stdout } else { [string]$stdout }
+        ExitCode = $exitCode
+    }
+}
 
-Write-Host "hello"
+function Get-PipxVersion {
+    $result = (Get-CommandResult "pipx --version").Output
+    $pipxVersion = $result -match "(?<version>\d+\.\d+\.\d+\.?\d*)" | Out-Null
+    return "Pipx $pipxVersion"
+}
+
+Get-PipxVersion
