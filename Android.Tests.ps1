ff8fb5d428abf1650769b9015ab51c7c1d8c02f3
Vladimir Safonkin <vsafonkin@gmail.com>
2020-12-21 00:05:30 +0300

    Update Android.Tests.ps1

--- Android.Tests.ps1
+++ Android.Tests.ps1
@@ -3,7 +3,7 @@ Import-Module "$PSScriptRoot/Common.Helpers.psm1"
 $os = Get-OSName
 
 Describe "Android" {
-    $androidSdkManagerPackages = Get-AndroidInstalledPackages
+    $androidSdkManagerPackages = Get-AndroidPackages
     [int]$platformMinVersion = Get-ToolsetValue "android.platform_min_version"
     [version]$buildToolsMinVersion = Get-ToolsetValue "android.build_tools_min_version"
 
