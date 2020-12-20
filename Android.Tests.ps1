ff156b784d01b4f742794b450481e25a9303a46b
Vladimir Safonkin <vsafonkin@gmail.com>
2020-12-21 00:28:24 +0300

    Update Android.Tests.ps1

--- Android.Tests.ps1
+++ Android.Tests.ps1
@@ -6,8 +6,6 @@ Describe "Android" {
     $androidSdkManagerPackages = Get-AndroidInstalledPackages
     [int]$platformMinVersion = Get-ToolsetValue "android.platform_min_version"
     [version]$buildToolsMinVersion = Get-ToolsetValue "android.build_tools_min_version"
-    
-    Write-Host "TEST"
 
     $platforms = (($androidSdkManagerPackages | Where-Object { "$_".StartsWith("platforms;") }) -replace 'platforms;', '' |
     Where-Object { [int]$_.Split("-")[1] -ge $platformMinVersion } | Sort-Object { [int]$_.Split("-")[1] } -Unique |
@@ -16,8 +14,6 @@ Describe "Android" {
     $buildTools = (($androidSdkManagerPackages | Where-Object { "$_".StartsWith("build-tools;") }) -replace 'build-tools;', '' |
     Where-Object { [version]$_ -ge $buildToolsMinVersion } | Sort-Object { [version]$_ } -Unique |
     ForEach-Object { "build-tools/${_}" })
-    
-    Write-Host "TEST 2"
 
     $androidPackages = @(
         "tools",
@@ -29,8 +25,6 @@ Describe "Android" {
         (Get-ToolsetValue "android.extra-list" | ForEach-Object { "extras/${_}" }),
         (Get-ToolsetValue "android.addon-list" | ForEach-Object { "add-ons/${_}" })
     ) | ForEach-Object { $_ }
-
-    Write-Host "TEST 3"
     
     BeforeAll {
         $ANDROID_SDK_DIR = "/usr/local/lib/android/sdk"
@@ -43,10 +37,9 @@ Describe "Android" {
 
             # Convert 'm2repository;com;android;support;constraint;constraint-layout-solver;1.0.0-beta1' ->
             #         'm2repository/com/android/support/constraint/constraint-layout-solver/1.0.0-beta1'
-#             $PackageName = $PackageName.Replace(";", "/")
-#             $targetPath = Join-Path $ANDROID_SDK_DIR $PackageName
-#             $targetPath | Should -Exist
-            Write-Host "HELLO"
+            $PackageName = $PackageName.Replace(";", "/")
+            $targetPath = Join-Path $ANDROID_SDK_DIR $PackageName
+            $targetPath | Should -Exist
         }
     }
     
