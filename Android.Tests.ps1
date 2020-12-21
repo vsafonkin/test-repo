278d53323494d3063ddd982d3aa2841b0aa10a28
Vladimir Safonkin <vsafonkin@gmail.com>
2020-12-21 09:16:07 +0300

    Update Android.Tests.ps1

--- Android.Tests.ps1
+++ Android.Tests.ps1
@@ -56,3 +56,21 @@ Describe "Android" {
 #         "$haxmPath -v" | Should -ReturnZeroExitCode
 #     }
 }
+
+Describe "Haskell" {
+    It "GHCup" {
+        "ghcup --version" | Should -ReturnZeroExitCode
+    }
+
+    It "GHC" {
+        "ghc --version" | Should -ReturnZeroExitCode
+    }
+
+    It "Cabal" {
+        "cabal --version" | Should -ReturnZeroExitCode
+    }
+
+    It "Stack" {
+        "stack --version" | Should -ReturnZeroExitCode
+    }
+}
