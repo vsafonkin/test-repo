5a645dbb6bda4ccf137b44a6f78f837102eeaab0
Vladimir Safonkin <vsafonkin@gmail.com>
2020-12-24 17:52:56 +0300

    Update Common.Helpers.psm1

--- Common.Helpers.psm1
+++ Common.Helpers.psm1
@@ -103,3 +103,40 @@ function ShouldReturnZeroExitCode {
         FailureMessage = $failureMessage
     }
 }
+
+function Invoke-PesterTests {
+    Param(
+        [Parameter(Mandatory)][string] $TestFile,
+        [string] $TestName
+    )
+
+    $testPath = "${TestFile}.Tests.ps1"
+    if (-not (Test-Path $testPath)) {
+        throw "Unable to find test file '$TestFile' on '$testPath'."
+    }
+
+    # Check that Pester module is imported
+    if (!(Get-Module "Pester")) {
+        Import-Module Pester
+    }
+    
+    $configuration = [PesterConfiguration] @{
+        Run = @{ Path = $testPath; PassThru = $true }
+        Output = @{ Verbosity = "Detailed" }
+    }
+    if ($TestName) {
+        $configuration.Filter.FullName = $TestName
+    }
+
+    # Switch ErrorActionPreference to Stop temporary to make sure that tests will fail on silent errors too
+    $backupErrorActionPreference = $ErrorActionPreference
+    $ErrorActionPreference = "Stop"
+    $results = Invoke-Pester -Configuration $configuration
+    $ErrorActionPreference = $backupErrorActionPreference
+
+    # Fail in case if no tests are run
+    if (-not ($results -and ($results.FailedCount -eq 0) -and ($results.PassedCount -gt 0))) {
+        $results
+        throw "Test run has failed"
+    }
+}
