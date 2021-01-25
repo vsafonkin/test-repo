dc98049e2830b48bf8d7fbeb50499ada5fd32ccf
Vladimir Safonkin <vsafonkin@gmail.com>
2021-01-25 10:33:23 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,4 +1,9 @@
-Get-ChildItem "$env:ANDROID_SDK_ROOT\ndk"
+
 
 $sdkManager = "$env:ANDROID_SDK_ROOT\tools\bin\sdkmanager.bat"
+& $sdkManager "ndk;21.3.6528147"
+& $sdkManager "ndk;22.0.7026061"
+
+Get-ChildItem "$env:ANDROID_SDK_ROOT\ndk"
+Write-Host "----------------"
 & $sdkManager --list
