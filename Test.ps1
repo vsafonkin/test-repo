9c2d595644c0cc4a7347ea63694dff4018a6a037
Vladimir Safonkin <vsafonkin@gmail.com>
2021-01-25 11:32:45 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,8 +1,8 @@
 
 
 $sdkManager = "$env:ANDROID_SDK_ROOT\tools\bin\sdkmanager.bat"
-& $sdkManager "ndk;21.3.6528147"
-& $sdkManager "ndk;22.0.7026061"
+& $sdkManager "ndk;21.3.6528147" | Out-Null
+& $sdkManager "ndk;22.0.7026061" | Out-Null
 
 Get-ChildItem "$env:ANDROID_SDK_ROOT\ndk"
 Write-Host "----------------"
