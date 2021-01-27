fda5f9cda6e443f2d4725192b77947a911f75c24
Vladimir Safonkin <vsafonkin@gmail.com>
2021-01-27 13:57:02 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,8 +1,28 @@
+function Get-AndroidSDKManagerPath {
+    return Join-Path $env:ANDROID_HOME "tools\bin\sdkmanager.bat"
+}
 
+function Get-AndroidAvailablePackages {
+    $androidSDKManagerPath = Get-AndroidSDKManagerPath
+    $androidSDKManagerList = & $androidSDKManagerPath --list --include_obsolete
+    $androidAvailablePackages = @()
+    $availablePackagesFlag = $false
+    foreach($packageInfo in $androidSDKManagerList) {
+        if(-not $availablePackagesFlag -and $packageInfo -Match "Available Packages:") {
+            $availablePackagesFlag = $true
+        }
+        if ($availablePackagesFlag) {
+            $androidAvailablePackages += $packageInfo
+        }
+    }
+    return $androidAvailablePackages
+}
 
-$sdkManager = "$env:ANDROID_SDK_ROOT\tools\bin\sdkmanager.bat"
-& $sdkManager "ndk;22" | Out-Null
+# $sdkManager = "$env:ANDROID_SDK_ROOT\tools\bin\sdkmanager.bat"
+# & $sdkManager "ndk;22" | Out-Null
 
-Get-ChildItem "$env:ANDROID_SDK_ROOT\ndk"
-Write-Host "----------------"
-& $sdkManager --list
+# Get-ChildItem "$env:ANDROID_SDK_ROOT\ndk"
+# Write-Host "----------------"
+# & $sdkManager --list
+
+Get-AndroidAvailablePackages
