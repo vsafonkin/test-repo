6211ac57dc3586c637d61184bc59846c1a917a93
Vladimir Safonkin <vsafonkin@gmail.com>
2021-01-28 18:34:36 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,4 +1,3 @@
-# Invoke-Expression "C:\msys64\mingw32\bin\gcc -v -m32 helloworld.c -o helloworld"
+Invoke-Expression "C:\msys64\mingw32\bin\gcc -v helloworld.c -o helloworld"
 Write-Host "------"
-New-Item -Path "C:\Users\RUNNER~1\AppData\Local\Temp" -Name "testfile1.txt" -ItemType "file" -Value "This is a text string."
 $LastExitCode
