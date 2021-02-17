7b05af2e68786cd4420f7740f01ea296d902c8e2
Vladimir Safonkin <vsafonkin@gmail.com>
2021-02-17 10:21:05 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,7 +1,1 @@
 [System.Net.Dns]::GetHostAddresses("jaraco.com") | foreach {Write-Host $_.IPAddressToString }
-Write-Host "-----"
-
-Install-Module DnsClient
-Import-Module DnsClient
-
-Resolve-DnsName -Name "jaraco.com"
