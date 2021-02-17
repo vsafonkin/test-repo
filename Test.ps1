c6fe1cb68bea30f6ef6c8245c1147e06516d63db
Vladimir Safonkin <vsafonkin@gmail.com>
2021-02-17 10:07:32 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,3 +1,2 @@
-Invoke-Expression "C:\msys64\mingw32\bin\gcc -v helloworld.c -o helloworld"
-Write-Host "------"
-$LastExitCode
+[System.Net.Dns]::GetHostAddresses("jaraco.com") | foreach {echo $_.IPAddressToString }
+
