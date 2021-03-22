2eae2d02959aedff119ac4811d9a0f5b20b4d99a
Vladimir Safonkin <vsafonkin@github.com>
2021-03-22 10:35:57 +0300

    Test

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Calculator Console Application" << endl << endl;
+    cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b"
+        << endl;
+    return 0;
+}
