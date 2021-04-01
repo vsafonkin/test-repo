8dcf6453e8490c1c059f1eed30d6aeb166e3ffa0
Vladimir Safonkin <vsafonkin@github.com>
2021-04-01 17:28:19 +0300

    Test

--- Test.ps1
+++ Test.ps1
@@ -1,1 +1,20 @@
-[System.Net.Dns]::GetHostAddresses("google.com") | foreach {Write-Host $_.IPAddressToString }
+function Take-OutputPart {
+    param (
+        [Parameter(ValueFromPipeline)]
+        [string] $toolOutput,
+        [string] $Delimiter = " ",
+        [int[]] $Part
+    )
+    $parts = $toolOutput.Split($Delimiter, [System.StringSplitOptions]::RemoveEmptyEntries)
+    $selectedParts = $parts[$Part]
+    return [string]::Join($Delimiter, $selectedParts)
+}
+
+function Get-AptSourceRepository {
+    param([String] $PackageName)
+
+    return &(apt show $PackageName) | Select-String -Pattern "APT-Sources" | Take-OutputPart -Part 1
+
+}
+
+Get-AptSourceRepository -PackageName 'postgresql'
