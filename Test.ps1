fb0cbb3111088dd01355077532c04bc19d929bde
Vladimir Safonkin <vsafonkin@gmail.com>
2021-12-10 12:57:36 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -15,10 +15,10 @@ function Run-Command {
 
 function Get-GccVersion {
     $versionList = @('9', '10', '11')
-    $versionList | Foreach-Object {
+    @($versionList | Foreach-Object {
         $version = Run-Command "gcc-${_} --version" | Select-Object -First 1
         "$version - available by ``gcc-${_}`` alias"
-    }
+    })
 }
 
 function Get-FortranVersion {
