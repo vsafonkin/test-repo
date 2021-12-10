1e0c374a690a97a3ea4af7fddd1b1bb7d96b73c6
Vladimir Safonkin <vsafonkin@gmail.com>
2021-12-10 12:20:17 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,1 +1,11 @@
-[System.Net.Dns]::GetHostAddresses("google.com") | foreach {Write-Host $_.IPAddressToString }
+# $gccList = @('9', '10', '11')
+
+function Get-GccVersion {
+    $versionList = @('9', '10', '11')
+    $versionList | Foreach-Object {
+        $version = Run-Command "gcc-${_} --version" | Select-Object -First 1
+        "$version - available by ``gcc-${_}`` alias"
+    }
+}
+
+Get-GccVersion
