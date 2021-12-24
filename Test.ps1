8e35c015df7114bd0d7a97c80d08f5456bb7329e
Vladimir Safonkin <vsafonkin@gmail.com>
2021-12-24 16:04:59 +0300

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,4 +1,5 @@
-For ($i = 0; $i -lt 50; $i++) {
+For ($i = 0; $i -lt 500; $i++) {
   $outfile = "dotnet-$i.zip"
   curl https://dotnetcli.azureedge.net/dotnet/Sdk/5.0.403/dotnet-sdk-5.0.403-win-x64.zip -o $outfile
+  rm $outfile
 }
