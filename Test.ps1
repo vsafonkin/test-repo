e447bb5db560d24c59e9d9c3644affca05dfef56
Vladimir Safonkin <vsafonkin@github.com>
2022-03-31 18:21:51 +0200

    Test

--- Test.ps1
+++ Test.ps1
@@ -1,5 +1,1 @@
-For ($i = 0; $i -lt 200; $i++) {
-  $outfile = "dotnet-$i.zip"
-  curl https://dotnetcli.azureedge.net/dotnet/Sdk/5.0.403/dotnet-sdk-5.0.403-win-x64.zip -o $outfile
-  rm $outfile
-}
+Write-Host 'Test'
