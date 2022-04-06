9a5ac0d7f841d28329c2f02ad4c93a148143ebea
Vladimir Safonkin <vsafonkin@github.com>
2022-04-06 13:36:22 +0200

    Test VS components

--- Test.ps1
+++ Test.ps1
@@ -1,1 +1,13 @@
-Write-Host 'Test AzDev'
+function Get-VisualStudioInstance {
+    # Use -Prerelease and -All flags to make sure that Preview versions of VS are found correctly
+    $vsInstance = Get-VSSetupInstance -Prerelease -All | Where-Object { $_.DisplayName -match "Visual Studio" } | Select-Object -First 1
+    $vsInstance | Select-VSSetupInstance -Product *
+}
+
+function Get-VisualStudioComponents {
+    (Get-VisualStudioInstance).Packages | Where-Object type -in 'Component', 'Workload' |
+    Sort-Object Id, Version | Select-Object @{n = 'Package'; e = {$_.Id}}, Version |
+    Where-Object { $_.Package -notmatch "[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}" }
+}
+
+Get-VisualStudioComponents
