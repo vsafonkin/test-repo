4eae0ee245ee47930e795a0bffff3b599cb7091d
Vladimir Safonkin <vsafonkin@github.com>
2022-04-10 18:58:29 +0200

    Test

--- sample.py
+++ sample.py
@@ -5,7 +5,7 @@ import sys
 # print(platform.system())
 # print(platform.release())
 # print(platform.version())
-print(sys.version_info)
+# print(sys.version_info)
 
-if sys.version_info >= (3, 10):
+if sys.version_info >= (3, 10) and platform.system() == 'Linux' and '18.04' in platform.version():
     print('It is true')
