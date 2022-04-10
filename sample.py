69030c22b21e11af87ef0ceeb095fff14cb7ea13
Vladimir Safonkin <vsafonkin@github.com>
2022-04-10 18:53:17 +0200

    Test

--- sample.py
+++ sample.py
@@ -1,6 +1,10 @@
 import platform
+import sys
 
-print("hello")
+print('hello')
 print(platform.system())
 print(platform.release())
 print(platform.version())
+
+if sys.version_info == (3, 10):
+    print('It is true')
