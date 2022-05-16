7752cb314948be7d6a4194113344fa97e49354e1
Vladimir Safonkin <vsafonkin@github.com>
2022-05-16 17:13:32 +0200

    Test tkinter

--- sample.py
+++ sample.py
@@ -1,11 +1,7 @@
-import platform
-import sys
+import tkinter as tk
 
-# print('hello')
-# print(platform.system())
-# print(platform.release())
-# print(platform.version())
-# print(sys.version_info)
-
-if sys.version_info >= (3, 10) and platform.system() == 'Linux' and '18.04' in platform.version():
-    print('It is true')
+root = tk.Tk()
+label = tk.Label(root, text="Hello World!")
+label.pack(padx=20, pady=20)
+root.after(5000, root.destroy)    # Close the Window after 5 seconds
+root.mainloop()
