f8ebe8e17d6be2440c14738e7014e99eb95924a0
Vladimir Safonkin <vsafonkin@github.com>
2022-05-19 15:55:31 +0200

    Test

--- sample.py
+++ sample.py
@@ -2,3 +2,11 @@ import tkinter as tk
 
 print('Tcl Version: ', tk.TclVersion)
 print('Tk Version', tk.TkVersion)
+
+print('---------')
+
+root = tk.Tk()
+label = tk.Label(root, text="Hello World!")
+label.pack(padx=20, pady=20)
+root.after(5000, root.destroy)    # Close the Window after 5 seconds
+root.mainloop()
