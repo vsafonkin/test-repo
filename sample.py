371d5c79babd61cf792d48db0bae9c10112332f6
Vladimir Safonkin <vsafonkin@github.com>
2022-05-19 17:57:24 +0200

    Test

--- sample.py
+++ sample.py
@@ -2,7 +2,7 @@ import tkinter as tk
 
 print('Tcl Version: ', tk.TclVersion)
 print('Tk Version', tk.TkVersion)
-print('Tk path', tk.getvar('tk_version'))
+print('Tk path', tk.Misc.getvar('tk_version'))
 
 print('---------')
 
