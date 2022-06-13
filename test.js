a1a1f257bd68e43a220f2de980e427576e7da212
Vladimir Safonkin <vsafonkin@github.com>
2022-06-13 13:58:39 +0200

    Test

--- test.js
+++ test.js
@@ -1,9 +1,8 @@
 "use strict";
 exports.__esModule = true;
-var semver = require("semver");
+var path = require("path");
 console.log('hello');
-var latestVersion = '1.2.3-rc1';
-var inputVersion = '3.x';
-console.log(semver.valid(inputVersion));
-var result = semver.satisfies(latestVersion, inputVersion, { includePrerelease: true });
-console.log(result);
+var myPath = "C:/python-version";
+if (path.isAbsolute(myPath)) {
+    console.log("Absolute", myPath);
+}
--- test.ts
+++ test.ts
@@ -1,11 +1,10 @@
-import * as semver from 'semver';
+import * as path from 'path';
 
 console.log('hello');
 
-const latestVersion = '1.2.3-rc1';
-const inputVersion = '3.x';
 
+const myPath = "C:/python-version"
 
-console.log(semver.valid(inputVersion));
-const result = semver.satisfies(latestVersion, inputVersion, { includePrerelease: true});
-console.log(result);
+if(path.isAbsolute(myPath)) {
+    console.log("Absolute", myPath)    
+}
