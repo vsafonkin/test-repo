9bce6fd20281f5887c8e6a94e4ef2708e8a036cb
Vladimir Safonkin <vsafonkin@github.com>
2022-08-10 09:53:45 +0200

    Test

--- test.js
+++ test.js
@@ -1,5 +1,11 @@
 "use strict";
+var _a;
 exports.__esModule = true;
-var semver = require("semver");
-var version = "16";
-console.log(semver.clean(version));
+var fs = require("fs");
+var content = fs.readFileSync("./package.json", 'utf8');
+// const found = content.match(/^(?:nodejs\s+)?v?(?<version>[^\s]+)$/m);
+// const groups = found?.groups
+// console.log(groups)
+// const nodeVersion = found?.groups?.version;
+var nodeVersion = (_a = JSON.parse(content).engines) === null || _a === void 0 ? void 0 : _a.node;
+console.log(nodeVersion);
--- test.ts
+++ test.ts
@@ -1,4 +1,12 @@
 import * as semver from 'semver';
+import * as fs from 'fs';
+import { version } from 'os';
 
-const version = "16";
-console.log(semver.clean(version))
+const content = fs.readFileSync("./package.json", 'utf8')
+
+// const found = content.match(/^(?:nodejs\s+)?v?(?<version>[^\s]+)$/m);
+// const groups = found?.groups
+// console.log(groups)
+// const nodeVersion = found?.groups?.version;
+const nodeVersion = JSON.parse(content).engines?.node;
+console.log(nodeVersion)
