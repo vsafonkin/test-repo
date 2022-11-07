d285ead44b07b291b5f119033a2528cf0c71ef82
vsafonkin <vsafonkin@gmail.com>
2022-11-07 22:12:31 +0100

    Test

--- test.ts
+++ test.ts
@@ -1,12 +1,1 @@
-import * as semver from 'semver';
-import * as fs from 'fs';
-import { version } from 'os';
-
-const content = fs.readFileSync("./package.json", 'utf8')
-
-// const found = content.match(/^(?:nodejs\s+)?v?(?<version>[^\s]+)$/m);
-// const groups = found?.groups
-// console.log(groups)
-// const nodeVersion = found?.groups?.version;
-const nodeVersion = JSON.parse(content).engines?.node;
-console.log(nodeVersion)
+console.log('hello')
